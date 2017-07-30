ce9ba31a56a79b4c1f8b1af99b3110730c98ac96
catrymer <cat@automattic.com>
2017-07-30 07:43:37 -0600

    Commit initial files
    Built this project locally. These are my completed files for the
    version 1.0 of this project.

---
+++ entertainment_center.py
@@ -0,0 +1,25 @@
+import media
+import cats_movies
+
+#instantiate the individual movie objects for the webpage
+mrs_henderson_presents = media.Movie("Mrs. Henderson Presents", "http://www.impawards.com/2005/posters/mrs_henderson_presents_ver2.jpg", "https://www.youtube.com/watch?v=X858gLx6kuc")
+
+paradise = media.Movie("The Paradise", "http://cdn-static.sidereel.com/tv_shows/53472/giant_2x/262585-4.jpg", "https://www.youtube.com/watch?v=M9etSsUgCnw")
+
+delovely = media.Movie("Delovely", "http://www.impawards.com/2004/posters/de_lovely_ver2.jpg", "https://www.youtube.com/watch?v=sjqQ1c9EJkY")
+
+moulin_rouge = media.Movie("Moulin Rouge", "https://s-media-cache-ak0.pinimg.com/736x/2b/af/b1/2bafb1c6a6b7660427859da54260c4ac--moulin-rouge-party-moulin-rouge-movie.jpg", "https://www.youtube.com/watch?v=dMSvKpVwavk")
+
+love_actually = media.Movie("Love Actually", "http://img.moviepostershop.com/love-actually-movie-poster-2003-1010326751.jpg", "https://www.youtube.com/watch?v=Wj4dHXsza0g")
+
+a_of_gg = media.Movie("Anne of Green Gables", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxNDAwMDg5MV5BMl5BanBnXkFtZTcwNzQ5ODA4Nw@@._V1._CR17,1,619,764_.jpg", "https://www.youtube.com/watch?v=czJi_FpLBYY")
+
+much_ado_nothing = media.Movie("Much Ado About Nothing", "http://img.moviepostershop.com/much-ado-about-nothing-movie-poster-1993-1010243486.jpg", "https://www.youtube.com/watch?v=__bPqeLkftc")
+
+titanic = media.Movie("Titanic", "https://s-media-cache-ak0.pinimg.com/736x/44/55/d9/4455d96357fb041d1cf3c8a5264ed593--titanic-leonardo-dicaprio-leonardo-dicaprio-kate-winslet.jpg", "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+
+thorn_birds = media.Movie("The Thorn Birds", "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA2MjQ4OTc1MjFeQTJeQWpwZ15BbWU4MDAzMDM5MDMx._V1_UX182_CR0,0,182,268_AL_.jpg", "https://www.youtube.com/watch?v=4ZP5D03LlV8")
+
+#add the movies to a list then open the movie page with the list content
+movies = [mrs_henderson_presents, paradise, delovely, moulin_rouge, love_actually, a_of_gg, much_ado_nothing, titanic, thorn_birds]
+cats_movies.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+#define a movie class with the necessary instance variables 
+class Movie():
+	"""This class stores information about movies."""
+
+	def __init__(self, movie_title, poster_image, youtube_trailer):
+		self.title = movie_title
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = youtube_trailer
+
+	#play the trailer
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
