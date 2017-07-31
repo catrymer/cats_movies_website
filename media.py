67633a51aa8465bbda2a4940f59ee98e37653d41
catrymer <cat@automattic.com>
2017-07-31 06:38:19 -0600

    Respond to Review
    Address the reviewer’s concerns about line length and adding a
    docstring to the __init__ function in my class.

--- media.py
+++ media.py
@@ -5,6 +5,7 @@ class Movie():
 	"""This class stores information about movies."""
 
 	def __init__(self, movie_title, poster_image, youtube_trailer):
+		"""The inputs for this class are movie_title, poster_image, and youtube_trailer"""
 		self.title = movie_title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = youtube_trailer
